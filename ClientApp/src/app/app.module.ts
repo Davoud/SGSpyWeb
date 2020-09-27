7782fd28c996a0c0d98ddc9648ccd434130bdece
Davoud <nas.david@yahoo.com>
2020-09-27 10:51:28 +0330

    Adde explorer

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -6,12 +6,15 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
+import { ExplorerComponent } from './rcomponents/explorer/explorer.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
-    HomeComponent,   
+    HomeComponent,
+    ExplorerComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
